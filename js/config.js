0ecece85b23bd80ac0444e8b27d1aef2654e68c2
nagapriyahebbare <hnagapriya017@gmail.com>
2026-05-09 12:34:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_bHR3Kgdyp',
-        userPoolClientId: '77cm2tgv0ibf8spfvjjjte4p9qu',
+        userPoolClientId: '5nakmceciionqfpmj3rndths8v',
         region: 'us-east-1'
     },
     api: {
